1d3f02ddf831d7855a6fcba1fc7acbeab0ae29fb
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2026-02-16 21:58:11 +0000

    Edit main.cpp

--- cpp-matrix-threadpool/main.cpp
+++ cpp-matrix-threadpool/main.cpp
@@ -9,6 +9,6 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "cpp-matrix-threadpool project started\n";
     return 0;
 }
