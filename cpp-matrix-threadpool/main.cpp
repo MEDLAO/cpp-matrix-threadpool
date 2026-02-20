4b7b99cf5f7fe4e7bf9f25bf29c7373e8fa7a937
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2026-02-20 17:54:22 +0000

    Add basic tests for Matrix element access

--- cpp-matrix-threadpool/main.cpp
+++ cpp-matrix-threadpool/main.cpp
@@ -6,9 +6,25 @@
 //
 
 #include <iostream>
+#include "Matrix.hpp"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
+    Matrix A(3, 4);
+    
+    // Creating the values
+    A.accessMatrix(2, 1) = 1.0;
+    A.accessMatrix(1, 4) = 7.0;
+    
+    // Reading the values
+    std::cout << A.accessMatrix(2, 1) << std::endl;
+    std::cout << A.accessMatrix(1, 4) << std::endl;
+    
+    const Matrix B(2, 3);
+    std::cout << B.accessMatrix(0, 0) << std::endl;
+    
+    // B.accessMatrix(0, 0) = 10.0;
+    
     std::cout << "cpp-matrix-threadpool project started\n";
     return 0;
 }
