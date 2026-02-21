2b75449073ce32f4bc83cfa171eb11729049e1a8
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2026-02-21 19:57:45 +0000

    Add matmul_baseline files

---
+++ cpp-matrix-threadpool/matmul_baseline.cpp
@@ -0,0 +1,8 @@
+//
+//  matmul_baseline.cpp
+//  cpp-matrix-threadpool
+//
+//  Created by MEDLAO on 21/02/2026.
+//
+
+#include "matmul_baseline.hpp"
---
+++ cpp-matrix-threadpool/matmul_baseline.hpp
@@ -0,0 +1,13 @@
+//
+//  matmul_baseline.hpp
+//  cpp-matrix-threadpool
+//
+//  Created by MEDLAO on 21/02/2026.
+//
+
+#ifndef matmul_baseline_hpp
+#define matmul_baseline_hpp
+
+#include <stdio.h>
+
+#endif /* matmul_baseline_hpp */
