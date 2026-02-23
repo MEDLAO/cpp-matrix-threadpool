0bb4436616ff25d0f604903d14d735c62535bd4e
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2026-02-23 16:33:27 +0000

    Add tests for matrix multiplication

--- cpp-matrix-threadpool/main.cpp
+++ cpp-matrix-threadpool/main.cpp
@@ -7,6 +7,8 @@
 
 #include <iostream>
 #include "Matrix.hpp"
+#include "matmul_baseline.hpp"
+
 
 int main(int argc, const char * argv[]) {
     
@@ -24,6 +26,45 @@ int main(int argc, const char * argv[]) {
     std::cout << B.accessMatrix(0, 0) << std::endl;
     
     // B.accessMatrix(0, 0) = 10.0;
+    
+    // Test multiplication
+    Matrix C(3, 2);
+    Matrix D(2, 4);
+    
+    // C first row
+    C.accessMatrix(0,0) = 1;
+    C.accessMatrix(0,1) = 2;
+    
+    // C second row
+    C.accessMatrix(1,0) = 3;
+    C.accessMatrix(1,1) = 4;
+
+    // C third row
+    C.accessMatrix(2,0) = 5;
+    C.accessMatrix(2,1) = 6;
+
+    // D first row
+    D.accessMatrix(0,0) = 7;
+    D.accessMatrix(0,1) = 8;
+    D.accessMatrix(0,2) = 9;
+    D.accessMatrix(0,3) = 10;
+
+    // D second row
+    D.accessMatrix(1,0) = 11;
+    D.accessMatrix(1,1) = 12;
+    D.accessMatrix(1,2) = 13;
+    D.accessMatrix(1,3) = 14;
+
+    
+    Matrix E = multiply(C, D);
+    
+    for (int i = 0; i < C.getRows(); i++) {
+        std::cout << "[ ";
+        for (int j = 0; j < D.getColumns(); j++) {
+            std::cout << E.accessMatrix(i, j) << " ";
+        }
+        std::cout << "]" << std::endl;
+    }
     
     std::cout << "cpp-matrix-threadpool project started\n";
     return 0;
