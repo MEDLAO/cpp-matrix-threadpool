c8555294878d3fc6006d42f895a129b1ea1a3628
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2026-03-02 21:55:20 +0000

    Add ThreadPool files

---
+++ cpp-matrix-threadpool/ThreadPool.cpp
@@ -0,0 +1,8 @@
+//
+//  ThreadPool.cpp
+//  cpp-matrix-threadpool
+//
+//  Created by MEDLAO on 02/03/2026.
+//
+
+#include "ThreadPool.hpp"
---
+++ cpp-matrix-threadpool/ThreadPool.hpp
@@ -0,0 +1,13 @@
+//
+//  ThreadPool.hpp
+//  cpp-matrix-threadpool
+//
+//  Created by MEDLAO on 02/03/2026.
+//
+
+#ifndef ThreadPool_hpp
+#define ThreadPool_hpp
+
+#include <stdio.h>
+
+#endif /* ThreadPool_hpp */
