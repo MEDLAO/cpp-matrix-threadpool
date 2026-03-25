8dc09591385111ba179ca8b8566cf3b3d8cc54c3
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2026-03-25 15:44:19 +0100

    Create result matrix for threadpool matrix multiplication

--- cpp-matrix-threadpool/matmul_threadpool.cpp
+++ cpp-matrix-threadpool/matmul_threadpool.cpp
@@ -14,5 +14,8 @@ Matrix multiply_threadpool(const Matrix& A, const Matrix& B)
 {
     if (A.getColumns() != B.getRows()) {
         throw std::invalid_argument("A.getColumns() must be equal to B.getRows()");
+        
+        Matrix C(A.getRows(), B.getColumns()); // result Matrix
+        
     }
 }
